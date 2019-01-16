9ab8557921aa76ad25cbdcc3c7ac223efc298996
angeline-n <nguyenangeli@seattleu.edu>
2019-01-15 20:48:52 -0800

    removed todos and created if/else for customers who want none of a thing

--- js/main.js
+++ js/main.js
@@ -1,40 +1,17 @@
 /* JavaScript for WATS 3020 Sandwich Machine Assignment */
 
-// Step One ///////////////////////////////////////////////////////////
-//
-// Gather data from the user using the prompt() command.
-// Users will fill in each prompt with text.
-// Provide examples where it may help (e.g. show a comma-separated list of
-// toppings to indicate how to specify toppings, etc.)
-//
-// TODO: Prompt the user for what kind of bread they would like.
-// Ideally, that would look something like: "What kind of bread (white, wheat, flat)?"
+
 let bread = prompt('What kind of bread (e.g.white, wheat, flat) would you like?');
 
 
-// TODO: Prompt the user for what kind of meat(s) they would like.
-// Indicate they should separate multiple items with a comma:
-// "What kind of meat? (Separate meats with a comma if you would like more than one.)"
-let meats = prompt('What kind of meat would you like? Separate meats with a comma if you would like more than one (e.g. bacon, turkey)?');
+let meats = prompt('What kind of meat would you like? Separate meats with a comma if you would like more than one (e.g. bacon, turkey)? If you would like none, enter "0".');
 
 
-// TODO: Prompt the user for what kind of toppings they would like.
-// We expect this to be multiple, so ask them to provide you with a
-// comma-separated list using a user friendly prompt.
-let toppings = prompt('What kind of toppings would you like? Separate toppings with a comma (e.g. cheddar cheese, lettuce, tomatoes).')
+let toppings = prompt('What kind of toppings would you like? Separate toppings with a comma (e.g. cheddar cheese, lettuce, tomatoes). If you would like none, enter "0".')
 
 
-// TODO: Prompt the user for what kind of condiments they would like.
-// Again, we should expect a comma-separated list if items here.
-let condiments = prompt('What kind of condiments would you like? Separate condiments with a comma (e.g. mayo, ketchup).')
+let condiments = prompt('What kind of condiments would you like? Separate condiments with a comma (e.g. mayo, ketchup). If you would like none, enter "0".')
 
-
-// Step Two ////////////////////////////////////////////////////////////
-//
-// Process all the input to calculate values for the user's order. The named
-// values are initialized to `null`. Change that so they equal the proper value.
-//
-// The `prices` object below specifies the prices for each thing.
 
 let prices = {
     sandwich: 5, // Base price for a sandwich is $5, includes bread
@@ -43,38 +20,38 @@ let prices = {
     condiment: 0.25 // Each condiment costs $0.25
 };
 
-// TODO: Convert order information from Strings to Arrays.
-
 let meatArray = meats.split(',');
 let toppingArray = toppings.split(',');
 let condimentArray = condiments.split(',');
 
-// TODO: Calculate cost for meat, toppings, and condiments.
-// This requires you to determine the length of each Array you just made
-// and multiply out the costs. You will need to refer to the attributes of the
-// `prices` object in order to calculate these costs.
+if (meats==='0'){
+    meatCost = 0;
+} else {
+    meatCost = meatArray.length * prices.meat; // while it currently costs 1 dollar and thus would be equal to the length, the price may change
+};
 
-let meatCost = meatArray.length * prices.meat; // while it currently costs 1 dollar and thus would be equal to the length, the price may change
-let toppingCost = toppingArray.length * prices.topping;
-let condimentCost = condimentArray.length * prices.condiment;
+if (toppings==='0'){
+    toppingCost = 0;
+} else {
+    toppingCost = toppingArray.length * prices.topping;
+};
 
-// TODO: Combine the costs of each part of the sandwich to get the subtotal.
+if (condiments==='0'){
+    condimentCost = 0;
+} else {
+    condimentCost = condimentArray.length * prices.condiment;
+};
+
+// Combine the costs of each part of the sandwich to get the subtotal.
 let subtotal = prices.sandwich + meatCost + toppingCost + condimentCost;
 
-// TODO: Calculate the tax owed using the waStateTaxRate.
+// Calculate the tax owed using the waStateTaxRate.
 let waStateTaxRate = 0.065;
 let orderTax = subtotal * waStateTaxRate;
 
-// TODO: Calculate `totalPrice` by adding `subtotal` and `orderTax`.
+// Calculate `totalPrice` by adding `subtotal` and `orderTax`.
 let totalPrice = subtotal + orderTax;
 
-
-// Step Three //////////////////////////////////////////////////////////
-//
-// TODO: Now that we've calculated all the values, insert them into this
-// template literal using proper expression tags. Note that we must provide
-// all of the info the user gave us to confirm the order, and then we must also
-// provide the cost information so the user understands their bill.
 
 let receiptTemplate = `
     <p>SANDWICH ORDER</p>
